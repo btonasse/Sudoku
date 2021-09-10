fda1d0014bf519557ea5d35e89219ea357432097
btonasse <33516406+btonasse@users.noreply.github.com>
2021-09-10 23:24:19 +0200

    make puzzle a state

--- Sudoku.py
+++ Sudoku.py
@@ -11,9 +11,9 @@ class Sudoku:
     Sudoku solver/generator. Uses a mixture of constraint propagation/backtracking to solve puzzles.
     # Todo usage
     '''
-    def __init__(self) -> None:
-        self.puzzles = None
-        self.solutions = None
+    def __init__(self, puzzle_string) -> None:
+        self.puzzle = self.parse_puzzle(puzzle_string)
+        self.solution = None
 
     def parse_puzzle(self, puzzle_string: str) -> list:
         '''
@@ -23,24 +23,26 @@ class Sudoku:
         '''
         if len(puzzle_string) != 81:
             raise ValueError(f'Puzzle string must have 81 characters, and not {len(puzzle_string)}.')
-        puzzle = []
+        if not all([char in '0123456789.' for char in puzzle_string]):
+            raise ValueError(f'Puzzle string only accepts digits. Use 0 or . for empty spaces.')
         puzzle_string = puzzle_string.replace('.', '0')
+        puzzle = []
         for i in range(0,81,9):
             puzzle.append([int(char) for char in puzzle_string[i:i+9]])
         return puzzle
     
-    def puzzle_to_notation(self, puzzle: list) -> str:
+    def puzzle_to_notation(self) -> str:
         '''
-        Transforms a puzzle into a string (with dots representing empty spaces)
+        Transforms the puzzle into a string (with dots representing empty spaces)
         '''
         string = ''
-        for row in puzzle:
+        for row in self.puzzle:
             for cell in row:
                 string += str(cell)
         string = string.replace('0','.')
         return string
 
-    def puzzle_to_string(self, puzzle: list) -> str:
+    def puzzle_to_string(self) -> str:
         '''
         Builds a string representation of the Sudoku grid for pretty printing.
         Replace 0's with whitespace.
@@ -49,7 +51,7 @@ class Sudoku:
         output = [separator]
         # Iterate three rows/cols at a time so separators can be inserted
         for y in range(0,9,3):
-            for row in puzzle[y:y+3]:
+            for row in self.puzzle[y:y+3]:
                 new_line = '| '
                 for x in range(0,9,3):
                     for cell in row[x:x+3]:
@@ -60,12 +62,12 @@ class Sudoku:
         output_string = '\n'.join(output)
         return output_string
 
-    def rows_to_cols(self, puzzle: list) -> list:
+    def rows_to_cols(self) -> list:
         '''
         Transpose the puzzle from a list of rows to a list of columns
         '''
-        return list(zip(*puzzle))
-    def rows_to_regions(self, puzzle: list) -> list:
+        return list(zip(*self.puzzle))
+    def rows_to_regions(self) -> list:
         '''
         Transpose the puzzle from a list of rows to a list of regions
         '''
@@ -74,33 +76,32 @@ class Sudoku:
             for col in range(0,9,3):
                 new_region = []
                 for i in range(3):
-                    new_region.extend(puzzle[row+i][col:col+3])
+                    new_region.extend(self.puzzle[row+i][col:col+3])
                 regions.append(new_region)
         return regions
                 
-    def is_possible(self, puzzle: list, row: int, col: int, number: int) -> bool:
+    def is_possible(self, row: int, col: int, number: int) -> bool:
         '''
         Check if a number can be entered in a given space.
             Args:
-                puzzle -> the whole grid
                 row/col -> the space coordinates
                 number -> the number being tested
         '''
         # Check if space already has a number
-        if puzzle[row][col]:
+        if self.puzzle[row][col]:
             return False
         # Check if number exists in row
-        if number in puzzle[row]:
+        if number in self.puzzle[row]:
             return False
         # Check if number exists in column
-        columns = self.rows_to_cols(puzzle)
+        columns = self.rows_to_cols()
         if number in columns[col]:
             return False
         # Check if number exists in region by:
         # 1) Transposing the puzzle to a list of regions
         # 2) Determine target space's coordinates in the 3x3 grid of regions
         # 3) Determine to which index of the new list these coordinates correspond
-        regions = self.rows_to_regions(puzzle)
+        regions = self.rows_to_regions()
         target_region = self.get_region_index(row, col)
         if number in regions[target_region]:
             return False
@@ -118,7 +119,7 @@ class Sudoku:
         region_index = region_row*3 + region_col
         return region_index
 
-    def get_possible_numbers_for_space(self, puzzle: list, row: int, col: int) -> list:
+    def get_possible_numbers_for_space(self, row: int, col: int) -> list:
         '''
         Get a list of possible numbers for a given space.
         Iterate through all 9 numbers and append them to the return list
@@ -128,12 +129,12 @@ class Sudoku:
                 row/col -> the space coordinates
         '''
         # If there's already a number, return a list with that as the single element
-        if puzzle[row][col]:
-            return [puzzle[row][col]]
+        if self.puzzle[row][col]:
+            return [self.puzzle[row][col]]
         
         possible_numbers = []
         for number in range(1,10):
-            if self.is_possible(puzzle, row, col, number):
+            if self.is_possible(row, col, number):
                 possible_numbers.append(number)
         
         if not possible_numbers:
@@ -141,20 +142,19 @@ class Sudoku:
 
         return possible_numbers
 
-    def get_possible_spaces_for_number(self, puzzle: list, row: int, col: int, number: int) -> List[tuple]:
+    def get_possible_spaces_for_number(self, row: int, col: int, number: int) -> List[tuple]:
         '''
         Get all possible spaces (represented as tuples of row/col coordinates),
         where a number can be placed in a given row, column and region.
             Args:
-                puzzle -> the full grid of the puzzle
                 row/col -> reference coordinate from which to derive the row, column and region to be analyzed
                 number -> number to analyze
             Returns:
                 Three lists of tuple coordinates (row, column and region),
                 representing all spaces where the specified number could be placed.
         '''
-        row_possibles = [(row, cell) for cell in range(9) if self.is_possible(puzzle, row, cell, number)]
-        col_possibles = [(cell, col) for cell in range(9) if self.is_possible(puzzle, cell, col, number)]
+        row_possibles = [(row, cell) for cell in range(9) if self.is_possible(row, cell, number)]
+        col_possibles = [(cell, col) for cell in range(9) if self.is_possible(cell, col, number)]
 
         region_root_x = (row//3)*3
         region_root_y = (col//3)*3
@@ -162,7 +162,7 @@ class Sudoku:
             (x,y)
             for x in range(region_root_x, region_root_x+3)
             for y in range(region_root_y, region_root_y+3)
-            if self.is_possible(puzzle, x, y, number)
+            if self.is_possible(x, y, number)
         ]
         
         return row_possibles, col_possibles, region_possibles
@@ -189,10 +189,17 @@ class Sudoku:
                     else:
                         # If a number cannot fit anywhere else in same row/column/region only has one possible space for a number, populate it here
                         for number in possibles:
-                            possibles_in_row, possibles_in_col, possibles_in_reg = self.get_possible_spaces_for_number(new_puzzle, row, col, number)
-                            if len(possibles_in_row) == 1 or len(possibles_in_col) == 1 or len(possibles_in_reg) == 1:
+                            in_row, in_col, in_reg = self.get_possible_spaces_for_number(new_puzzle, row, col, number)
+                            if len(in_row) == 1 or len(in_col) == 1 or len(in_reg) == 1:
                                 new_puzzle[row][col] = number
-                            
+        
+        # Check if new_puzzle is the same as original one (no more propagation is possible)
+        # If it is not, try to keep propagating recursively
+        if new_puzzle == puzzle:
+            return new_puzzle
+        else:
+            return self.constraint_propagation(new_puzzle)
+
 
 
 
--- tests/test_Sudoku.py
+++ tests/test_Sudoku.py
@@ -1,11 +1,19 @@
+'''
+Unit tests for sudoku solver/creator
+Usage:
+python -m unittest discover -s tests -v
+'''
 import unittest
 from Sudoku import Sudoku
 
 class TestSudoku(unittest.TestCase):
 
     def setUp(self) -> None:
-        self.sud = Sudoku()
-        self.puzzle = [
+        self.sud = Sudoku('003020600900305001001806400008102900700000008006708200002609500800203009005010300')
+    
+    def test_puzzle_parsing(self) -> None:
+        puzzle_string = '003020600900305001001806400008102900700000008006708200002609500800203009005010300'
+        expected_puzzle = [
             [0, 0, 3, 0, 2, 0, 6, 0, 0],
             [9, 0, 0, 3, 0, 5, 0, 0, 1],
             [0, 0, 1, 8, 0, 6, 4, 0, 0],
@@ -16,26 +24,23 @@ class TestSudoku(unittest.TestCase):
             [8, 0, 0, 2, 0, 3, 0, 0, 9],
             [0, 0, 5, 0, 1, 0, 3, 0, 0]
         ]
-    
-    def test_puzzle_parsing(self) -> None:
-        puzzle_string = '003020600900305001001806400008102900700000008006708200002609500800203009005010300'
-        puzzle = self.sud.parse_puzzle(puzzle_string)
-        self.assertEqual(puzzle, self.puzzle)
+        sudoku = Sudoku(puzzle_string)
+        self.assertEqual(sudoku.puzzle, expected_puzzle)
 
     def test_printable_string(self) -> None:
         expected = '+-------+-------+-------+\n|     3 |   2   | 6     | \n| 9     | 3   5 |     1 | \n|     1 | 8   6 | 4     | \n+-------+-------+-------+\n|     8 | 1   2 | 9     | \n| 7     |       |     8 | \n|     6 | 7   8 | 2     | \n+-------+-------+-------+\n|     2 | 6   9 | 5     | \n| 8     | 2   3 |     9 | \n|     5 |   1   | 3     | \n+-------+-------+-------+'
-        puzzle_as_string = self.sud.puzzle_to_string(self.puzzle)
+        puzzle_as_string = self.sud.puzzle_to_string()
         self.assertEqual(puzzle_as_string, expected)
     
     def test_puzzle_to_notation(self) -> None:
         expected = '..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3..'
-        notation = self.sud.puzzle_to_notation(self.puzzle)
+        notation = self.sud.puzzle_to_notation()
         self.assertEqual(notation, expected)
 
     def test_transposition(self) -> None:
         expected_cols = [(0, 9, 0, 0, 7, 0, 0, 8, 0), (0, 0, 0, 0, 0, 0, 0, 0, 0), (3, 0, 1, 8, 0, 6, 2, 0, 5), (0, 3, 8, 1, 0, 7, 6, 2, 0), (2, 0, 0, 0, 0, 0, 0, 0, 1), (0, 5, 6, 2, 0, 8, 9, 3, 0), (6, 0, 4, 9, 0, 2, 5, 0, 3), (0, 0, 0, 0, 0, 0, 0, 0, 0), (0, 1, 0, 0, 8, 0, 0, 9, 0)]
         expected_regions = [[0, 0, 3, 9, 0, 0, 0, 0, 1], [0, 2, 0, 3, 0, 5, 8, 0, 6], [6, 0, 0, 0, 0, 1, 4, 0, 0], [0, 0, 8, 7, 0, 0, 0, 0, 6], [1, 0, 2, 0, 0, 0, 7, 0, 8], [9, 0, 0, 0, 0, 8, 2, 0, 0], [0, 0, 2, 8, 0, 0, 0, 0, 5], [6, 0, 9, 2, 0, 3, 0, 1, 0], [5, 0, 0, 0, 0, 9, 3, 0, 0]]
-        cols = self.sud.rows_to_cols(self.puzzle)
-        regions = self.sud.rows_to_regions(self.puzzle)
+        cols = self.sud.rows_to_cols()
+        regions = self.sud.rows_to_regions()
         self.assertEqual(cols, expected_cols)
         self.assertEqual(regions, expected_regions)
